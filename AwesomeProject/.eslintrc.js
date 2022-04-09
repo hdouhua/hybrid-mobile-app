4aa4f10703ac20174047096f98b72a9e437dc518
hdouhua <yilin.dao@gmail.com>
2022-04-09 11:35:23 +0800

    [add]: support typescript, eslint for react-hooks

--- AwesomeProject/.eslintrc.js
+++ AwesomeProject/.eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:react-hooks/recommended'],
+  plugins: ['react-hooks'],
   rules: {
     'react-native/no-inline-styles': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
+  parser: 'babel-eslint',
 };
