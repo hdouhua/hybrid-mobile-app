dc6cf2eea2905a7cb086cb4eb50a1112e9e066db
hdouhua <yilin.dao@gmail.com>
2022-04-14 00:36:04 +0800

    [fix]: bug on image prefetch

--- AwesomeProject/src/c05/NetworkImage.jsx
+++ AwesomeProject/src/c05/NetworkImage.jsx
@@ -2,13 +2,18 @@ import React, {useEffect} from 'react';
 import {View, Image, Text} from 'react-native';
 
 import {Styles} from './Styles';
+import DefaultImage from './DefaultImage';
 
 export default function NetworkImage() {
   let imageUri = 'https://d2zp5xs5cp8zlg.cloudfront.net/image-30938-800.jpg';
   useEffect(() => {
-    Image.prefetch(imageUri, () => {
-      console.log('image loaded');
-    });
+    Image.prefetch(imageUri)
+      .then(() => {
+        console.log('image loaded');
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, [imageUri]);
 
   return (
@@ -17,6 +22,7 @@ export default function NetworkImage() {
       <Image
         style={Styles.img}
         source={{uri: imageUri, cache: 'only-if-cached'}}
+        defaultSource={{uri: DefaultImage}}
       />
     </View>
   );
