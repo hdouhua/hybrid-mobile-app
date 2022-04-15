d2e65679833ed4494c0ab79f086eb37a0ddd4b65
hdouhua <yilin.dao@gmail.com>
2022-04-15 23:27:26 +0800

    [fix]: Alert

--- AwesomeProject/src/c05/StaticImage.jsx
+++ AwesomeProject/src/c05/StaticImage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, Alert} from 'react-native';
 import {Styles} from './Styles';
 
 const cat = require('../res/cat_cute.jpeg');
 
 export default function StaticImage() {
   //可以通过 Image.resolveAssetSource 方法来获取图片信息
-  // alert(JSON.stringify(Image.resolveAssetSource(cat)));
+  Alert.alert(JSON.stringify(Image.resolveAssetSource(cat)));
 
   return (
     <View style={Styles.container}>
