ac10a53d7286f7744585e17236c1d6f9d46599ab
hdouhua <yilin.dao@gmail.com>
2022-05-09 12:26:39 +0800

    [fix]: eslint for typescript

--- AwesomeProject/.eslintrc.js
+++ AwesomeProject/.eslintrc.js
@@ -1,11 +1,19 @@
 module.exports = {
   root: true,
-  plugins: ['react-hooks'],
-  extends: ['@react-native-community', 'plugin:react-hooks/recommended'],
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@react-native-community',
+    'plugin:react-hooks/recommended',
+  ],
   rules: {
     'react-native/no-inline-styles': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
-  parser: 'babel-eslint',
+  parser: '@typescript-eslint/parser',
 };
