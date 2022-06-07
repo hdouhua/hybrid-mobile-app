131f1fa26d67615c5d17048c7369f3810912203a
hdouhua <yilin.dao@gmail.com>
2022-06-07 11:59:20 +0800

    [add]: add module-resolver

--- AwesomeProject/babel.config.js
+++ AwesomeProject/babel.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ts',
+          '.tsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.jpg',
+          '.png',
+          '.svg',
+          '.webp',
+        ],
+        alias: {
+          '@asset': './src/assets',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+  ],
 };
