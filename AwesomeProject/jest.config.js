3ccda05dd0bdb0c86b3f411a956079eeb79663e4
hdouhua <hdouhua@gmail.com>
2023-07-25 22:57:07 +0800

    fix: small issues

--- AwesomeProject/jest.config.js
+++ AwesomeProject/jest.config.js
@@ -1,6 +1,9 @@
 const config = {
   verbose: true,
   preset: 'react-native',
+  setupFiles: [
+    '<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
   testEnvironment: 'jsdom',
   testEnvironmentOptions: {
     customExportConditions: ['react-native'],
@@ -14,6 +17,16 @@ const config = {
         tsconfig: 'tsconfig.spec.json',
       },
     ],
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/node_modules/react-native/jest/assetFileTransformer.js',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(@react-|react-native|@sentry))'],
+  // moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src'],
+  moduleNameMapper: {
+    '@app-json': '<rootDir>/app.json',
+    '@asset/(.*)': '<rootDir>/src/assets/$1',
+    '@shared/(.*)': '<rootDir>/src/shared/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
 
