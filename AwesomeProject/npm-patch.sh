9c729d432bc09677c3228ce0f07d736f32289edc
hdouhua <hdouhua@gmail.com>
2023-07-25 21:01:37 +0800

    fix[@react-navigation/drawer]: patch for bug fix on hooks of React-Native-Reanimated(https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/web-support/#web-without-a-babel-plugin)

--- AwesomeProject/npm-patch.sh
+++ AwesomeProject/npm-patch.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
-# patch package recyclerlistview
-cd node_modules/recyclerlistview
-npm run build
-cd -
-npx patch-package recyclerlistview
+# # patch package recyclerlistview
+# cd node_modules/recyclerlistview
+# npm run build
+# cd -
+# npx patch-package recyclerlistview
 
-# patch package metro-runtime
-npx patch-package metro-runtime
+# # patch package metro-runtime
+# npx patch-package metro-runtime
+
+# patch package @react-navigation/drawer
+cd node_modules/@react-navigation/drawer
+mv lib lib-bak
+npm install
+npm run prepack
+cp lib/commonjs/views/modern/Drawer.js lib-bak/commonjs/views/modern/
+cp lib/module/views/modern/Drawer.js lib-bak/module/views/modern/
+rm -rf lib
+mv lib-bak lib
+rm -rf node_modules package-lock.json
+cd -
+npx patch-package @react-navigation/drawer
+
+#
