f386b6f63df608c5bedd98bbc6e0b456751b830b
hdouhua <hdouhua@gmail.com>
2023-07-27 17:01:25 +0800

    fix: rename tsconfig for jest

--- AwesomeProject/jest.config.js
+++ AwesomeProject/jest.config.js
@@ -14,7 +14,7 @@ const config = {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
-        tsconfig: 'tsconfig.spec.json',
+        tsconfig: 'tsconfig.jest.json',
       },
     ],
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
